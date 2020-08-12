13e24e5631da8ac7d7fe62358b2836fac296c153
Yunet <lbvfzx@gmail.com>
2020-08-12 14:48:05 +0300

    ADDDDDDDDDD MAPPPPPPP

--- JavaScript/settingsMap.js
+++ JavaScript/settingsMap.js
@@ -1,7 +1,7 @@
- ymaps.ready(init);
- function init(){
-    var myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 7
-        });
-    }
+ymaps.ready(init);
+		function init(){
+			var moscow_map = new ymaps.Map("first_map", {
+					center: [55.76, 37.64],
+					zoom: 10
+				});
+		}
