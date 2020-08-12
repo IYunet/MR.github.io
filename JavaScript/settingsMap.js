f69ff25da7263a74d96efc1925fc008d145d94f6
Yunet <lbvfzx@gmail.com>
2020-08-12 21:40:42 +0300

    ADD FISRT

--- JavaScript/settingsMap.js
+++ JavaScript/settingsMap.js
@@ -1,7 +1,30 @@
 ymaps.ready(init);
 		function init(){
-			var moscow_map = new ymaps.Map("first_map", {
-					center: [55.76, 37.64],
-					zoom: 10
+			var map = new ymaps.Map("first_map", {
+					center: [52.284387, 104.279504],
+					zoom: 3
 				});
+		
+		
+			var pointA = [52.293568, 104.286110],
+			pointB = [52.291487, 104.280185],
+			pointC = [52.287963, 104.280718],
+        /**
+         * Создаем мультимаршрут.
+         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRoute.xml
+         */
+			multiRoute = new ymaps.multiRouter.MultiRoute({
+				referencePoints: [
+					pointA,
+					pointB,
+					pointC				],
+				params: {
+                //Тип маршрутизации - пешеходная маршрутизация.
+					routingMode: 'pedestrian'
+				}
+			}, 	{
+            // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
+            boundsAutoApply: true
+			});
+			map.geoObjects.add(multiRoute);
 		}
